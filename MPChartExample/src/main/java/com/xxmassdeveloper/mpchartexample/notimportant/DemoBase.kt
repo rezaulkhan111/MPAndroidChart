575aea2c31f837ece2aea8e8e5aba03b236583a6
Rezaul_Khan <rezaulkhan111@hotmail.com>
2022-10-24 22:49:44 +0600

    update java to kotlin app

--- MPChartExample/src/main/java/com/xxmassdeveloper/mpchartexample/notimportant/DemoBase.kt
+++ MPChartExample/src/main/java/com/xxmassdeveloper/mpchartexample/notimportant/DemoBase.kt
@@ -1,99 +1,156 @@
+package com.xxmassdeveloper.mpchartexample.notimportant
 
-package com.xxmassdeveloper.mpchartexample.notimportant;
-
-import android.Manifest;
-import android.content.pm.PackageManager;
-import android.graphics.Typeface;
-import android.os.Bundle;
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import com.google.android.material.snackbar.Snackbar;
-
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.app.ActivityCompat;
-import android.view.View;
-import android.widget.Toast;
-
-import com.github.mikephil.charting.charts.Chart;
-import com.xxmassdeveloper.mpchartexample.R;
+import android.Manifest
+import com.github.mikephil.charting.charts.Chart.saveToGallery
+import com.github.mikephil.charting.utils.Utils.init
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat.OnRequestPermissionsResultCallback
+import android.graphics.Typeface
+import android.os.Bundle
+import com.xxmassdeveloper.mpchartexample.R
+import com.xxmassdeveloper.mpchartexample.notimportant.DemoBase
+import android.content.pm.PackageManager
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import com.google.android.material.snackbar.Snackbar
+import com.github.mikephil.charting.charts.Chart
+import com.xxmassdeveloper.mpchartexample.notimportant.ContentItem
+import android.widget.ArrayAdapter
+import android.annotation.SuppressLint
+import android.view.ViewGroup
+import android.view.LayoutInflater
+import android.widget.TextView
+import android.widget.AdapterView.OnItemClickListener
+import android.view.WindowManager
+import com.xxmassdeveloper.mpchartexample.notimportant.MyAdapter
+import android.widget.AdapterView
+import android.content.Intent
+import android.view.View
+import com.xxmassdeveloper.mpchartexample.LineChartActivity1
+import com.xxmassdeveloper.mpchartexample.MultiLineChartActivity
+import com.xxmassdeveloper.mpchartexample.LineChartActivity2
+import com.xxmassdeveloper.mpchartexample.InvertedLineChartActivity
+import com.xxmassdeveloper.mpchartexample.CubicLineChartActivity
+import com.xxmassdeveloper.mpchartexample.LineChartActivityColored
+import com.xxmassdeveloper.mpchartexample.PerformanceLineChart
+import com.xxmassdeveloper.mpchartexample.FilledLineActivity
+import com.xxmassdeveloper.mpchartexample.BarChartActivity
+import com.xxmassdeveloper.mpchartexample.AnotherBarActivity
+import com.xxmassdeveloper.mpchartexample.BarChartActivityMultiDataset
+import com.xxmassdeveloper.mpchartexample.HorizontalBarChartActivity
+import com.xxmassdeveloper.mpchartexample.StackedBarActivity
+import com.xxmassdeveloper.mpchartexample.BarChartPositiveNegative
+import com.xxmassdeveloper.mpchartexample.HorizontalBarNegativeChartActivity
+import com.xxmassdeveloper.mpchartexample.StackedBarActivityNegative
+import com.xxmassdeveloper.mpchartexample.BarChartActivitySinus
+import com.xxmassdeveloper.mpchartexample.PieChartActivity
+import com.xxmassdeveloper.mpchartexample.PiePolylineChartActivity
+import com.xxmassdeveloper.mpchartexample.HalfPieChartActivity
+import com.xxmassdeveloper.mpchartexample.CombinedChartActivity
+import com.xxmassdeveloper.mpchartexample.ScatterChartActivity
+import com.xxmassdeveloper.mpchartexample.BubbleChartActivity
+import com.xxmassdeveloper.mpchartexample.CandleStickChartActivity
+import com.xxmassdeveloper.mpchartexample.RadarChartActivity
+import com.xxmassdeveloper.mpchartexample.ListViewMultiChartActivity
+import com.xxmassdeveloper.mpchartexample.fragments.SimpleChartDemo
+import com.xxmassdeveloper.mpchartexample.ScrollViewActivity
+import com.xxmassdeveloper.mpchartexample.ListViewBarChartActivity
+import com.xxmassdeveloper.mpchartexample.DynamicalAddingActivity
+import com.xxmassdeveloper.mpchartexample.RealtimeLineChartActivity
+import com.xxmassdeveloper.mpchartexample.LineChartTime
 
 /**
  * Base class of all Activities of the Demo Application.
  *
  * @author Philipp Jahoda
  */
-public abstract class DemoBase extends AppCompatActivity implements ActivityCompat.OnRequestPermissionsResultCallback {
-
-    protected final String[] months = new String[] {
-            "Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Okt", "Nov", "Dec"
-    };
-
-    protected final String[] parties = new String[] {
-            "Party A", "Party B", "Party C", "Party D", "Party E", "Party F", "Party G", "Party H",
-            "Party I", "Party J", "Party K", "Party L", "Party M", "Party N", "Party O", "Party P",
-            "Party Q", "Party R", "Party S", "Party T", "Party U", "Party V", "Party W", "Party X",
-            "Party Y", "Party Z"
-    };
-
-    private static final int PERMISSION_STORAGE = 0;
-
-    protected Typeface tfRegular;
-    protected Typeface tfLight;
-
-    @Override
-    protected void onCreate(@Nullable Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-
-        tfRegular = Typeface.createFromAsset(getAssets(), "OpenSans-Regular.ttf");
-        tfLight = Typeface.createFromAsset(getAssets(), "OpenSans-Light.ttf");
+abstract class DemoBase : AppCompatActivity(), OnRequestPermissionsResultCallback {
+    @JvmField
+    protected val months = arrayOf(
+        "Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Okt", "Nov", "Dec"
+    )
+    @JvmField
+    protected val parties = arrayOf(
+        "Party A", "Party B", "Party C", "Party D", "Party E", "Party F", "Party G", "Party H",
+        "Party I", "Party J", "Party K", "Party L", "Party M", "Party N", "Party O", "Party P",
+        "Party Q", "Party R", "Party S", "Party T", "Party U", "Party V", "Party W", "Party X",
+        "Party Y", "Party Z"
+    )
+    @JvmField
+    protected var tfRegular: Typeface? = null
+    @JvmField
+    protected var tfLight: Typeface? = null
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        tfRegular = Typeface.createFromAsset(assets, "OpenSans-Regular.ttf")
+        tfLight = Typeface.createFromAsset(assets, "OpenSans-Light.ttf")
     }
 
-    protected float getRandom(float range, float start) {
-        return (float) (Math.random() * range) + start;
+    protected fun getRandom(range: Float, start: Float): Float {
+        return (Math.random() * range).toFloat() + start
     }
 
-    @Override
-    public void onBackPressed() {
-        super.onBackPressed();
-        overridePendingTransition(R.anim.move_left_in_activity, R.anim.move_right_out_activity);
+    override fun onBackPressed() {
+        super.onBackPressed()
+        overridePendingTransition(R.anim.move_left_in_activity, R.anim.move_right_out_activity)
     }
 
-    @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<String>,
+        grantResults: IntArray
+    ) {
         if (requestCode == PERMISSION_STORAGE) {
-            if (grantResults.length == 1 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                saveToGallery();
+            if (grantResults.size == 1 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                saveToGallery()
             } else {
-                Toast.makeText(getApplicationContext(), "Saving FAILED!", Toast.LENGTH_SHORT)
-                        .show();
+                Toast.makeText(applicationContext, "Saving FAILED!", Toast.LENGTH_SHORT)
+                    .show()
             }
         }
     }
 
-    protected void requestStoragePermission(View view) {
-        if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.WRITE_EXTERNAL_STORAGE)) {
-            Snackbar.make(view, "Write permission is required to save image to gallery", Snackbar.LENGTH_INDEFINITE)
-                    .setAction(android.R.string.ok, new View.OnClickListener() {
-                        @Override
-                        public void onClick(View v) {
-                            ActivityCompat.requestPermissions(DemoBase.this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, PERMISSION_STORAGE);
-                        }
-                    }).show();
+    protected fun requestStoragePermission(view: View?) {
+        if (ActivityCompat.shouldShowRequestPermissionRationale(
+                this,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE
+            )
+        ) {
+            Snackbar.make(
+                view!!,
+                "Write permission is required to save image to gallery",
+                Snackbar.LENGTH_INDEFINITE
+            )
+                .setAction(android.R.string.ok) {
+                    ActivityCompat.requestPermissions(
+                        this@DemoBase, arrayOf(
+                            Manifest.permission.WRITE_EXTERNAL_STORAGE
+                        ), PERMISSION_STORAGE
+                    )
+                }
+                .show()
         } else {
-            Toast.makeText(getApplicationContext(), "Permission Required!", Toast.LENGTH_SHORT)
-                    .show();
-            ActivityCompat.requestPermissions(DemoBase.this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, PERMISSION_STORAGE);
+            Toast.makeText(applicationContext, "Permission Required!", Toast.LENGTH_SHORT)
+                .show()
+            ActivityCompat.requestPermissions(
+                this@DemoBase,
+                arrayOf(Manifest.permission.WRITE_EXTERNAL_STORAGE),
+                PERMISSION_STORAGE
+            )
         }
     }
 
-    protected void saveToGallery(Chart chart, String name) {
-        if (chart.saveToGallery(name + "_" + System.currentTimeMillis(), 70))
-            Toast.makeText(getApplicationContext(), "Saving SUCCESSFUL!",
-                    Toast.LENGTH_SHORT).show();
-        else
-            Toast.makeText(getApplicationContext(), "Saving FAILED!", Toast.LENGTH_SHORT)
-                    .show();
+    protected fun saveToGallery(chart: Chart<*>, name: String) {
+        if (chart.saveToGallery(name + "_" + System.currentTimeMillis(), 70)) Toast.makeText(
+            applicationContext, "Saving SUCCESSFUL!",
+            Toast.LENGTH_SHORT
+        ).show() else Toast.makeText(applicationContext, "Saving FAILED!", Toast.LENGTH_SHORT)
+            .show()
     }
 
-    protected abstract void saveToGallery();
+    protected abstract fun saveToGallery()
+
+    companion object {
+        private const val PERMISSION_STORAGE = 0
+    }
 }
--- MPChartExample/src/main/java/com/xxmassdeveloper/mpchartexample/notimportant/MyAdapter.kt
+++ MPChartExample/src/main/java/com/xxmassdeveloper/mpchartexample/notimportant/MyAdapter.kt
@@ -1,71 +1,101 @@
-package com.xxmassdeveloper.mpchartexample.notimportant;
+package com.xxmassdeveloper.mpchartexample.notimportant
 
-import android.annotation.SuppressLint;
-import android.content.Context;
-import android.graphics.Typeface;
-import androidx.annotation.NonNull;
-
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
-import android.widget.TextView;
-
-import com.xxmassdeveloper.mpchartexample.R;
-
-import java.util.List;
+import com.github.mikephil.charting.charts.Chart.saveToGallery
+import com.github.mikephil.charting.utils.Utils.init
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat.OnRequestPermissionsResultCallback
+import android.graphics.Typeface
+import android.os.Bundle
+import com.xxmassdeveloper.mpchartexample.R
+import com.xxmassdeveloper.mpchartexample.notimportant.DemoBase
+import android.content.pm.PackageManager
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import com.google.android.material.snackbar.Snackbar
+import com.github.mikephil.charting.charts.Chart
+import com.xxmassdeveloper.mpchartexample.notimportant.ContentItem
+import android.widget.ArrayAdapter
+import android.annotation.SuppressLint
+import android.content.Context
+import android.view.ViewGroup
+import android.view.LayoutInflater
+import android.widget.TextView
+import android.widget.AdapterView.OnItemClickListener
+import android.view.WindowManager
+import com.xxmassdeveloper.mpchartexample.notimportant.MyAdapter
+import android.widget.AdapterView
+import android.content.Intent
+import android.view.View
+import com.xxmassdeveloper.mpchartexample.LineChartActivity1
+import com.xxmassdeveloper.mpchartexample.MultiLineChartActivity
+import com.xxmassdeveloper.mpchartexample.LineChartActivity2
+import com.xxmassdeveloper.mpchartexample.InvertedLineChartActivity
+import com.xxmassdeveloper.mpchartexample.CubicLineChartActivity
+import com.xxmassdeveloper.mpchartexample.LineChartActivityColored
+import com.xxmassdeveloper.mpchartexample.PerformanceLineChart
+import com.xxmassdeveloper.mpchartexample.FilledLineActivity
+import com.xxmassdeveloper.mpchartexample.BarChartActivity
+import com.xxmassdeveloper.mpchartexample.AnotherBarActivity
+import com.xxmassdeveloper.mpchartexample.BarChartActivityMultiDataset
+import com.xxmassdeveloper.mpchartexample.HorizontalBarChartActivity
+import com.xxmassdeveloper.mpchartexample.StackedBarActivity
+import com.xxmassdeveloper.mpchartexample.BarChartPositiveNegative
+import com.xxmassdeveloper.mpchartexample.HorizontalBarNegativeChartActivity
+import com.xxmassdeveloper.mpchartexample.StackedBarActivityNegative
+import com.xxmassdeveloper.mpchartexample.BarChartActivitySinus
+import com.xxmassdeveloper.mpchartexample.PieChartActivity
+import com.xxmassdeveloper.mpchartexample.PiePolylineChartActivity
+import com.xxmassdeveloper.mpchartexample.HalfPieChartActivity
+import com.xxmassdeveloper.mpchartexample.CombinedChartActivity
+import com.xxmassdeveloper.mpchartexample.ScatterChartActivity
+import com.xxmassdeveloper.mpchartexample.BubbleChartActivity
+import com.xxmassdeveloper.mpchartexample.CandleStickChartActivity
+import com.xxmassdeveloper.mpchartexample.RadarChartActivity
+import com.xxmassdeveloper.mpchartexample.ListViewMultiChartActivity
+import com.xxmassdeveloper.mpchartexample.fragments.SimpleChartDemo
+import com.xxmassdeveloper.mpchartexample.ScrollViewActivity
+import com.xxmassdeveloper.mpchartexample.ListViewBarChartActivity
+import com.xxmassdeveloper.mpchartexample.DynamicalAddingActivity
+import com.xxmassdeveloper.mpchartexample.RealtimeLineChartActivity
+import com.xxmassdeveloper.mpchartexample.LineChartTime
 
 /**
  * Created by Philipp Jahoda on 07/12/15.
  */
-class MyAdapter extends ArrayAdapter<ContentItem> {
-
-    private final Typeface mTypeFaceLight;
-    private final Typeface mTypeFaceRegular;
-
-    MyAdapter(Context context, List<ContentItem> objects) {
-        super(context, 0, objects);
-
-        mTypeFaceLight = Typeface.createFromAsset(context.getAssets(), "OpenSans-Light.ttf");
-        mTypeFaceRegular = Typeface.createFromAsset(context.getAssets(), "OpenSans-Regular.ttf");
-    }
-
+internal class MyAdapter(context: Context, objects: List<ContentItem>?) :
+    ArrayAdapter<ContentItem?>(context, 0, objects!!) {
+    private val mTypeFaceLight: Typeface
+    private val mTypeFaceRegular: Typeface
     @SuppressLint("InflateParams")
-    @NonNull
-    @Override
-    public View getView(int position, View convertView, @NonNull ViewGroup parent) {
-
-        ContentItem c = getItem(position);
-
-        ViewHolder holder;
-
-        holder = new ViewHolder();
-
-        if (c != null && c.isSection) {
-            convertView = LayoutInflater.from(getContext()).inflate(R.layout.list_item_section, null);
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
+        var convertView = convertView
+        val c = getItem(position)
+        val holder: ViewHolder
+        holder = ViewHolder()
+        convertView = if (c != null && c.isSection) {
+            LayoutInflater.from(context).inflate(R.layout.list_item_section, null)
         } else {
-            convertView = LayoutInflater.from(getContext()).inflate(R.layout.list_item, null);
+            LayoutInflater.from(context).inflate(R.layout.list_item, null)
         }
-
-        holder.tvName = convertView.findViewById(R.id.tvName);
-        holder.tvDesc = convertView.findViewById(R.id.tvDesc);
-
-        convertView.setTag(holder);
-
-        if (c != null && c.isSection)
-            holder.tvName.setTypeface(mTypeFaceRegular);
-        else
-            holder.tvName.setTypeface(mTypeFaceLight);
-        holder.tvDesc.setTypeface(mTypeFaceLight);
-
-        holder.tvName.setText(c != null ? c.name : null);
-        holder.tvDesc.setText(c != null ? c.desc : null);
-
-        return convertView;
+        holder.tvName = convertView.findViewById(R.id.tvName)
+        holder.tvDesc = convertView.findViewById(R.id.tvDesc)
+        convertView.tag = holder
+        if (c != null && c.isSection) holder.tvName.setTypeface(mTypeFaceRegular) else holder.tvName.setTypeface(
+            mTypeFaceLight
+        )
+        holder.tvDesc.setTypeface(mTypeFaceLight)
+        holder.tvName.setText(c?.name)
+        holder.tvDesc.setText(c?.desc)
+        return convertView
     }
 
-    private class ViewHolder {
+    private inner class ViewHolder {
+        var tvName: TextView? = null
+        var tvDesc: TextView? = null
+    }
 
-        TextView tvName, tvDesc;
+    init {
+        mTypeFaceLight = Typeface.createFromAsset(context.assets, "OpenSans-Light.ttf")
+        mTypeFaceRegular = Typeface.createFromAsset(context.assets, "OpenSans-Regular.ttf")
     }
 }
