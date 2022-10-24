1d2d43013290ec0f180157433004c1ab40691cd9
Rezaul_Khan <rezaulkhan111@hotmail.com>
2022-10-24 23:17:41 +0600

    clean code

--- MPChartExample/src/main/java/com/xxmassdeveloper/mpchartexample/custom/YearXAxisFormatter.kt
+++ MPChartExample/src/main/java/com/xxmassdeveloper/mpchartexample/custom/YearXAxisFormatter.kt
@@ -1,34 +1,7 @@
 package com.xxmassdeveloper.mpchartexample.custom
 
-import com.github.mikephil.charting.utils.Utils.formatNumber
-import com.github.mikephil.charting.data.CandleEntry.high
-import com.github.mikephil.charting.data.BaseEntry.y
-import com.github.mikephil.charting.components.MarkerView.refreshContent
-import com.github.mikephil.charting.formatter.IAxisValueFormatter.getFormattedValue
-import com.github.mikephil.charting.data.Entry.x
-import com.github.mikephil.charting.charts.BarLineChartBase.visibleXRange
-import com.github.mikephil.charting.data.BarEntry.yVals
-import com.github.mikephil.charting.highlight.Highlight.stackIndex
-import com.github.mikephil.charting.data.BarEntry.y
-import com.github.mikephil.charting.interfaces.datasets.IScatterDataSet.scatterShapeSize
-import com.github.mikephil.charting.utils.ViewPortHandler.scaleX
-import android.annotation.SuppressLint
-import com.github.mikephil.charting.components.MarkerView
-import android.widget.TextView
-import com.github.mikephil.charting.data.CandleEntry
-import com.github.mikephil.charting.utils.MPPointF
-import com.xxmassdeveloper.mpchartexample.R
-import com.github.mikephil.charting.formatter.IAxisValueFormatter
-import com.github.mikephil.charting.formatter.IFillFormatter
-import com.github.mikephil.charting.interfaces.datasets.ILineDataSet
-import com.github.mikephil.charting.interfaces.dataprovider.LineDataProvider
-import android.graphics.Typeface
-import com.github.mikephil.charting.formatter.IValueFormatter
-import com.github.mikephil.charting.utils.ViewPortHandler
 import com.github.mikephil.charting.components.AxisBase
-import com.github.mikephil.charting.charts.BarLineChartBase
-import com.github.mikephil.charting.data.BarEntry
-import com.github.mikephil.charting.interfaces.datasets.IScatterDataSet
+import com.github.mikephil.charting.formatter.IAxisValueFormatter
 
 /**
  * Created by Philipp Jahoda on 14/09/15.
